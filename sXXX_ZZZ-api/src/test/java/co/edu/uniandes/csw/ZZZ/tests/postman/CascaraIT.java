953d1eb3c4b1e4d3c459fc9fe877a567675a134a
José Bocanegra <josejbocanegra@gmail.com>
2020-01-24 21:14:00 -0500

    Update CascaraIT.java

--- sXXX_ZZZ-api/src/test/java/co/edu/uniandes/csw/ZZZ/tests/postman/CascaraIT.java
+++ sXXX_ZZZ-api/src/test/java/co/edu/uniandes/csw/ZZZ/tests/postman/CascaraIT.java
@@ -74,12 +74,7 @@ public class CascaraIT {
         PostmanTestBuilder tp = new PostmanTestBuilder();
         tp.setTestWithoutLogin(COLLECTION, "Entorno-IT.postman_environment");
         String desiredResult = "0";
-        Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult, tp.getIterations_failed());
-
         Assert.assertEquals("Error en Requests de: " + COLLECTION, desiredResult, tp.getRequests_failed());
-
-        Assert.assertEquals("Error en Test-Scripts de: " + COLLECTION, desiredResult, tp.getTest_scripts_failed());
-
         Assert.assertEquals("Error en Assertions de: " + COLLECTION, desiredResult, tp.getAssertions_failed());
     }
 }
