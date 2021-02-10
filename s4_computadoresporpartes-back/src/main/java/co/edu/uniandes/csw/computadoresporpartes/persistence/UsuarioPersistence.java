bcf61751f178fb32c7db44aa0860585520f077fa
sepenuela <69486519+sepenuela@users.noreply.github.com>
2021-02-09 20:02:04 -0500

    Taller básico persistencia

---
+++ s4_computadoresporpartes-back/src/main/java/co/edu/uniandes/csw/computadoresporpartes/persistence/UsuarioPersistence.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.computadoresporpartes.persistence;
+
+import co.edu.uniandes.csw.computadoresporpartes.entities.UsuarioEntity;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Sergio Peñuela
+ */
+@Stateless
+public class UsuarioPersistence {
+    
+    public UsuarioEntity create (UsuarioEntity usuario)
+    {
+        throw new UnsupportedOperationException("Not yet supported");
+    }
+    
+}
